fb819ceaf2f154c6b337dbf1c98a851824e0c3b9
Jathurchan Selvakumar <admin@jathurchan.com>
2025-02-07 19:54:34 +0100

    feat: Set up Next.js project

---
+++ src/app/layout.tsx
@@ -0,0 +1,36 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "DiveCode.io",
+  description: "System for consistent, collaborative learning, \
+  showcasing skills, and providing guidance to others who are \
+  tackling similar challenges.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
