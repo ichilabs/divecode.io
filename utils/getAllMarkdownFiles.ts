2cc4d28da4401ee26d3015a24014b21fde253f02
Jathurchan Selvakumar <admin@jathurchan.com>
2025-02-08 15:05:55 +0100

    feat: Get all markdown files in a directory

---
+++ utils/getAllMarkdownFiles.ts
@@ -0,0 +1,47 @@
+// utils/getAllMarkdownFiles.ts
+
+import * as fs from 'fs';
+import * as path from 'path';
+
+interface getAllMarkdownFilesOptions {
+    /**
+     * List of filenames (case-insensitive) to skip/ignore.
+     * e.g. ['index.md']
+     */
+    ignoreFilenames?: string[]
+}
+
+
+export function getAllMarkdownFiles(
+    dirPath: string,
+    options: getAllMarkdownFilesOptions = {}
+): string[] {
+
+    // `[]` is the default value if called without `ignoreFilenames`
+    const { ignoreFilenames = [] } = options;
+
+    const filePaths: string[] = [];
+
+    function processDirectory(currPath: string) {
+        const dirEntries = fs.readdirSync(currPath, { withFileTypes: true });
+
+        for (const dirent of dirEntries) {
+            const fullPath = path.join(currPath, dirent.name);
+
+            if (dirent.isDirectory()) {
+                processDirectory(fullPath);
+            } else if (dirent.isFile() && dirent.name.toLowerCase().endsWith('.md')) {
+                // Skip if it's in the ignore list
+                if (ignoreFilenames.map(f => f.toLowerCase()).includes(dirent.name.toLowerCase())) {
+                    continue;
+                }
+
+                filePaths.push(fullPath)
+            }
+        }
+
+    }
+
+    processDirectory(dirPath)
+    return filePaths
+}
